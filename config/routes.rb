68be591bbc1c469ce782c6a3cdfe93245f52d8fa
ok <ok@gee.ks.ua>
2014-11-20 01:46:51 +0200

    weird routes problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   get '/ukrnet_rss(/:filter)' => 'ukrnet_rss/posts#index'
-  get '/yandex_rss' => 'yandex_rss/rss_items#index'
 end
--- lib/ukrnet_rss/engine.rb
+++ lib/ukrnet_rss/engine.rb
@@ -1,3 +1,3 @@
-module MyGem
+module UkrnetRss
   class Engine < Rails::Engine; end
 end
